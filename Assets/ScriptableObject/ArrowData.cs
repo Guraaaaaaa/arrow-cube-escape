4dc3345bff2915420032dc755fe2070c37f38774
Binhmin <lebinhminh674@gmail.com>
2026-04-18 17:05:14 +0700

    firt commit

--- Assets/ScriptableObject/ArrowData.cs
+++
@@ -1,8 +0,0 @@
-using UnityEngine;
-
-[CreateAssetMenu(fileName = "ArrowData", menuName = "Scriptable Objects/ArrowData")]
-public class ArrowData : ScriptableObject
-{
-    public ArrowDirections.Direction direction;
-    public Vector2Int position;
-}
